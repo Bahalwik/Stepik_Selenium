033c715d368790539b4602e6d1d0b6cec6eccc9e
Александр <name_sasha@mail.ru>
2025-10-03 17:36:53 +0300

    add test login button

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Укажите язык: ru, en, es, fr и т.д.')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(10)
+    yield browser
+    browser.quit()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
+
 
 class MainPage(BasePage):
 
@@ -8,3 +8,5 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+    def should_be_login_link(self):
+        assert self.is_element_present(By.ID, "login_link"), "Login link is not presented"
